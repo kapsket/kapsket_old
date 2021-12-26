2c5305ee011b8298a704cfdf3b6d59680f00429d
Haskaleikur <poddevin.romain@gmail.com>
2021-12-26 21:06:30 +0100

    migrations pending

--- db/schema.rb
+++ db/schema.rb
@@ -88,11 +88,17 @@ ActiveRecord::Schema.define(version: 2020_12_08_102910) do
   create_table "orders", force: :cascade do |t|
     t.string "stripe_id"
     t.string "status"
-    t.datetime "paid_at"
+    t.float "amount"
+    t.text "city"
+    t.text "country"
+    t.text "line1"
+    t.text "line2"
+    t.text "postal_code"
+    t.text "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "address_id"
     t.bigint "user_id"
+    t.bigint "address_id"
     t.index ["address_id"], name: "index_orders_on_address_id"
     t.index ["stripe_id"], name: "index_orders_on_stripe_id", unique: true
     t.index ["user_id"], name: "index_orders_on_user_id"
