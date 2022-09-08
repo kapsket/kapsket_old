55c64a28416a140b9f60469f99e5c06a974564be
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-08 17:49:35 +0200

    Upgrade to Rails 7.0.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to:  'products#index'
+  resources :line_items
+  resources :breweries, only: [:show, :index] 
+  resources :contact, only: [:index]
+  resources :about, only: [:index]
+  resources :orders
+  resources :caps, only: [:show, :index]
+  resources :colors, only: [:show, :index]
+  resources :products, only: [:show, :create, :index]
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  resources :carts
+  
+  resources :webhooks, only: [:create] 
+
+  scope '/checkout' do 
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+  end
+
+  namespace :admin do 
+    root to: 'products#index'
+    resources :orders 
+    resources :types
+    resources :caps  do
+      resources :images, only: [:create]
+      end
+    resources :colors 
+    resources :products do
+      resources :photos, only: [:create]
+      end
+    resources :users do 
+      resources :carts 
+    end
+    resources :types 
+    resources :breweries  
+    end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_06_142759) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_152600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -57,36 +56,36 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
     t.text "name"
     t.text "location"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "caps", force: :cascade do |t|
     t.text "name"
     t.string "company"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "colors", force: :cascade do |t|
     t.text "name"
     t.string "material"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "line_items", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
@@ -95,9 +94,9 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
   create_table "orders", force: :cascade do |t|
     t.string "stripe_id"
     t.string "status"
-    t.datetime "paid_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "paid_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "address_id"
     t.bigint "user_id"
     t.index ["address_id"], name: "index_orders_on_address_id"
@@ -110,8 +109,8 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
     t.float "price"
     t.text "description"
     t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "color_id"
     t.bigint "cap_id"
     t.bigint "type_id"
@@ -122,8 +121,8 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
 
   create_table "types", force: :cascade do |t|
     t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -132,13 +131,13 @@ ActiveRecord::Schema.define(version: 2022_09_06_142759) do
     t.string "phone"
     t.string "stripe_id"
     t.boolean "is_admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.boolean "guest", default: false
     t.bigint "address_id"
     t.index ["address_id"], name: "index_users_on_address_id"
